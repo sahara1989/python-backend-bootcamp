4eeb4924107d1c12253331147e68c09fd3ebddca
sahara1989 <Viktorrybakov1989@gmail.com>
2025-07-29 15:45:47 +0200

    Настройка Flask для деплоя на Render

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def api_delete_task(index):
     return jsonify({'error': 'Invalid index'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port) 
