ad42bf7324767e99de9a6cf321c7661acced68bd
sahara1989 <Viktorrybakov1989@gmail.com>
2025-08-04 17:31:30 +0200

    Fix templates path

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,8 @@ csrf = CSRFProtect()
 login_manager = LoginManager()
 
 def create_app():
-    base_dir = os.path.abspath(os.path.dirname(__file__))
-    template_dir = os.path.join(base_dir, "..", "templates")
-
-    app = Flask(__name__, template_folder=template_dir)
+  
+    app = Flask(__name__)
     
     # остальные настройки...
 
