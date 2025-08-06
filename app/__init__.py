072ee082d4572344d22e6cc7b7bce15460924186
sahara1989 <Viktorrybakov1989@gmail.com>
2025-08-06 17:22:57 +0200

    Init Flask-Migrate and add github_id to User

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
+from flask_migrate import Migrate
 
 from .config import DevelopmentConfig, ProductionConfig
 from flask_dance.contrib.github import make_github_blueprint, github
@@ -11,6 +12,7 @@ from flask_dance.contrib.github import make_github_blueprint, github
 db = SQLAlchemy()
 csrf = CSRFProtect()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
 
@@ -27,9 +29,10 @@ def create_app():
 
     # 3. Инициализация расширений
     db.init_app(app)
+    migrate.init_app(app, db)
     csrf.init_app(app)
     login_manager.init_app(app)
-    
+
         # --- GitHub OAuth ---
     from flask_dance.contrib.github import make_github_blueprint
     github_bp = make_github_blueprint(
