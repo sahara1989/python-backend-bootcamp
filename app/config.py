2c1bd5bf96a59b7de83369ddc51e5d4c4bee8104
sahara1989 <Viktorrybakov1989@gmail.com>
2025-08-07 16:53:08 +0200

    Восстановлены миграции без удаления БД

--- app/config.py
+++ app/config.py
@@ -21,7 +21,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     @staticmethod
     def fix_postgres_uri(uri):
